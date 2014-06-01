39716d1f30456b90d3029cd4dcc766d04a24a97f
derrick3192 <derrickfutschik@hotmail.com>
2014-06-01 14:19:36 +1000

    Fixed the implicit conversion.

--- src/com/derrick/linearscala/symblic/SymbolicVar.scala
+++ src/com/derrick/linearscala/symblic/SymbolicVar.scala
@@ -2,24 +2,30 @@ package com.derrick.linearscala.symoblic {
 
   import collection.immutable.HashMap
   import com.sun.xml.internal.bind.v2.runtime.IllegalAnnotationsException
-  import com.derrick.linearscala.symoblic._
+  import com.derrick.linearscala.symoblic.SymbolicVarImplicits._
+  
+  
 
   object SymbolicVar {
     def apply(x: Double) = new SymbolicVar(x)
     def apply(v: Map[String, Double]) = new SymbolicVar(v)
+    def apply(v: String) = new SymbolicVar(v)
   }
 
-  class SymbolicVar(val _variables: Map[String, Double], val _numeric: Double) {
+  case class SymbolicVar(val _variables: Map[String, Double], val _numeric: Double, val _isNumeric: Boolean = false) {
 
-    def variables = _variables;
-    def numeric = _numeric;
+    def variables: Map[String, Double] = _variables;
+    def numeric: Double = _numeric;
+    def isNumeric: Boolean = _isNumeric;
 
     def apply(v: Map[String, Double]) = new SymbolicVar(v)
     def apply(x: Double) = new SymbolicVar(x)
 
-    def this(x: Double) = this(Map(), x)
+    def this(x: Double) = this(Map(), x, true)
     def this(v: Map[String, Double]) = this(v, 0.0)
+    def this(v: String) = this(Map(v->1.0), 0.0, false)
 
+    
     def +(that: SymbolicVar): SymbolicVar =
       {
         val variables = SymbolicVar.this.variables ++ that.variables.map { case (k, v) => k -> (v + SymbolicVar.this.variables.getOrElse(k, 0.0)) }
@@ -35,51 +41,75 @@ package com.derrick.linearscala.symoblic {
       }
 
     def *(that: SymbolicVar): SymbolicVar = {
-      if (SymbolicVar.this.variables.size != 0) {
-        return new SymbolicVar(SymbolicVar.this.variables, SymbolicVar.this.numeric * that.numeric)
-      } else if (that.variables.size != 0) {
-        return new SymbolicVar(that.variables, SymbolicVar.this.numeric * that.numeric)
+      if (that.isNumeric || that.variables.size == 0) {
+        return this * that.numeric
+      } else if (this.isNumeric || this.variables.size == 0) {
+        return that * this.numeric
       } else {
         throw new IllegalArgumentException("One of the symbolics is not a numeric")
       }
     }
 
     def /(that: SymbolicVar): SymbolicVar = {
-      if (SymbolicVar.this.variables.size != 0) {
-        return new SymbolicVar(SymbolicVar.this.variables, SymbolicVar.this.numeric / that.numeric)
-      } else if (that.variables.size != 0) {
-        return new SymbolicVar(that.variables, SymbolicVar.this.numeric / that.numeric)
+      if (that.isNumeric || that.variables.size == 0) {
+        return this / that.numeric
+      } else if (this.isNumeric || this.variables.size == 0) {
+        return that / this.numeric
       } else {
         throw new IllegalArgumentException("One of the symbolics is not a numeric")
       }
     }
 
-    def *(that: Double): SymbolicVar = return new SymbolicVar(SymbolicVar.this.variables, SymbolicVar.this.numeric * that)
+    def *(that: Double): SymbolicVar = return new SymbolicVar(transformVariable((x:Double) => x * that), SymbolicVar.this.numeric * that)
+    
+    def /(that: Double): SymbolicVar = return new SymbolicVar(transformVariable((x:Double) => x / that), SymbolicVar.this.numeric / that)
 
     def +(that: Double): SymbolicVar = return new SymbolicVar(SymbolicVar.this.variables, SymbolicVar.this.numeric + that)
 
     def -(that: Double): SymbolicVar = return new SymbolicVar(SymbolicVar.this.variables, SymbolicVar.this.numeric - that)
 
-    def /(that: Double): SymbolicVar = return new SymbolicVar(SymbolicVar.this.variables, SymbolicVar.this.numeric / that)
+
 
     def variablesToString: String = ((for (m <- SymbolicVar.this.variables) yield " " + m._2 + "*" + m._1).toString())
 
     override def toString: String = return SymbolicVar.this.numeric + "+" + SymbolicVar.this.variablesToString
 
+    private def transformVariable(func: (Double) => Double): Map[String,Double] = {
+     return _variables.map( k=>(k._1,func(k._2)))
+    }
+    
+
   }
 
-  object SymbolicImplicits {
-    implicit def Double2Symbolic(x: Double) = new SymbolicVar(x)
+  object SymbolicVarImplicits {
+    implicit def Double2Symbolic(x: Double):SymbolicVar = new SymbolicVar(x)
   }
 
   object Main {
+    
+    
+    
     def main(args: Array[String]) {
-    	val x = SymbolicVar(2.0)
-    	val y = SymbolicVar(30.2)
-    	
-    	val z:SymbolicVar = x * 3.00 + 2.9
-    	
-    	println(z)
+//      val x = SymbolicVar(2.0)
+//      val y = SymbolicVar(30.2)
+//
+//      val z: SymbolicVar = x * 3.00 + 2.9
+//      println(z)
+//
+//      val zz = new SymbolicVar(Map("x" -> 3.0))
+//      val zy = new SymbolicVar(Map("x" -> 32.00, "y" -> 23.0))
+//
+//      println(zz + zy * 3.0)
+      
+      val x = SymbolicVar("x")
+      val x2 = SymbolicVar("x") * 2.0
+      println(2.0*x-2.0-2*x+2.0)
+    }
+  }
+
+  class Main {
+    def main(args: Array[String]) {
+      Main.main(args)
     }
   }
 
